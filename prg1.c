65ba6aab8e4efcdd9024d99dd775e7980b26c7fb
Afzal Sorathiya <afzalsorathiya19@gmail.com>
2022-09-16 21:54:18 +0530

    first commit

---
+++ prg1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// sum of 1 to 10 numbers
+// void main()
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         sum = i + sum;
+//     }
+//     printf("The sum is = %d",sum);
+// }
+
+void  main()
+{
+    /* code */
+    int sal[] = {10,20,30,40,50,60,70,80,90,100},sum;
+
+    sal[2] = 31;
+
+
+    // for (int i = 0; i <= 9; i++)
+    // {
+    //     /* code */
+    //     sum = sal[i] + sum;
+    // }
+    
+    // printf("%d",sum);
+
+    printf("%d",sal[2]);
+}
+
